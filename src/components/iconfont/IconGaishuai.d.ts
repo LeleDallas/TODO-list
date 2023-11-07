7a69fb036eee59a9f5de9261c81d4543e4d14462
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-11-07 10:31:16 +0100

    chore(Iconfont): setup icons

---
+++ src/components/iconfont/IconGaishuai.d.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+
+import { SVGAttributes, FunctionComponent } from 'react';
+
+interface Props extends Omit<SVGAttributes<SVGElement>, 'color'> {
+  size?: number;
+  color?: string | string[];
+}
+
+declare const IconGaishuai: FunctionComponent<Props>;
+
+export default IconGaishuai;
