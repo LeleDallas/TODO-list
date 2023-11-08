19209439695b4926e90eaad134c21cc557c59328
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-11-08 14:38:25 +0100

    refactor: pages with global state and localstorage

--- src/screen/Settings.tsx
+++ src/screen/Settings.tsx
@@ -2,14 +2,20 @@ import CustomPageContainer from "../components/Layout/CustomPageContainer"
 import { Button, Col, Divider, Popconfirm, Row, Switch } from "antd"
 import { AccountSubTitle, GreyParagraph } from "../components/Components"
 import { QuestionCircleOutlined } from "@ant-design/icons"
+import { useState } from "react"
+import UserFrom from "../components/Modal/UserFrom"
 
 
 type SettingsProps = {
     theme: boolean
     setTheme: (theme: boolean) => void
+    username: string,
+    setUsername: (username: string) => void
 }
 
-const Settings = ({ theme, setTheme }: SettingsProps) => {
+const Settings = ({ theme, setTheme, username, setUsername }: SettingsProps) => {
+    const [visible, setVisible] = useState(false)
+
     return (
         <CustomPageContainer title="Settings">
             <>
@@ -20,8 +26,8 @@ const Settings = ({ theme, setTheme }: SettingsProps) => {
                         <AccountSubTitle>Theme</AccountSubTitle>
                         <GreyParagraph>Edit program theme.</GreyParagraph>
                     </Col>
-                    <Col style={{ marginRight: "20px" }}>
-                        <Switch data-testid="switch" defaultChecked={theme} onChange={(val) => setTheme(val)} />
+                    <Col style={{ marginRight: "10px" }}>
+                        <Switch defaultChecked={theme} onChange={(val) => setTheme(val)} />
                     </Col>
                 </Row>
                 <Divider />
@@ -30,8 +36,8 @@ const Settings = ({ theme, setTheme }: SettingsProps) => {
                         <AccountSubTitle>Notifications</AccountSubTitle>
                         <GreyParagraph>Edit push notifications.</GreyParagraph>
                     </Col>
-                    <Col style={{ marginRight: "20px" }}>
-                        <Switch data-testid="switch" />
+                    <Col style={{ marginRight: "10px" }}>
+                        <Switch />
                     </Col>
                 </Row>
                 <Divider />
@@ -43,6 +49,7 @@ const Settings = ({ theme, setTheme }: SettingsProps) => {
                     <Col>
                         <Button
                             type="primary"
+                            onClick={() => setVisible(true)}
                             style={{ borderRadius: 6, height: 40 }}>
                             Change
                         </Button>
@@ -50,16 +57,24 @@ const Settings = ({ theme, setTheme }: SettingsProps) => {
                 </Row>
                 <Divider />
                 <Row justify="space-between" align="middle">
-                    <Col span={15}>
+                    <Col span={14}>
                         <AccountSubTitle>Delete all TODO</AccountSubTitle>
                         <GreyParagraph>Remove your tasks from the network.</GreyParagraph>
                     </Col>
                     <Col >
-                        <Popconfirm icon={<QuestionCircleOutlined style={{ color: 'red' }} />} title="Are you sure? This action is not reversible" okText="Yes" cancelText="No" onConfirm={() => console.log("")}>
-                            <Button danger style={{ borderRadius: 6, marginRight: "20px", height: 40 }}>Delete</Button>
+                        <Popconfirm
+                            icon={<QuestionCircleOutlined style={{ color: 'red' }} />}
+                            title="Are you sure? This action is not reversible"
+                            okText="Yes"
+                            cancelText="No"
+                            onConfirm={() => console.log("")}
+                        >
+                            <Button danger style={{ borderRadius: 6, height: 40 }}>Delete</Button>
                         </Popconfirm>
                     </Col>
-                </Row></>
+                </Row>
+                <UserFrom visible={visible} setVisible={setVisible} username={username} setUsername={setUsername} />
+            </>
         </CustomPageContainer>
     )
 }
--- src/screen/Task.tsx
+++ src/screen/Task.tsx
@@ -1,8 +1,15 @@
+import { AccountSubTitle } from "../components/Components"
 import CustomPageContainer from "../components/Layout/CustomPageContainer"
 
 const Task = () => {
     return (
-        <CustomPageContainer title="Task List" children={<></>} />
+        <CustomPageContainer title="Task List" >
+            <>
+                <AccountSubTitle>Today</AccountSubTitle>
+                <AccountSubTitle>Tomorrow</AccountSubTitle>
+                <AccountSubTitle>Upcoming</AccountSubTitle>
+            </>
+        </CustomPageContainer>
     )
 }
 export default Task
