2a1f8e43896a67be0fec4137e322db6a2c6049c9
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-11-08 12:25:25 +0100

    feat(Layout): default page layout

---
+++ src/components/Layout/CustomPageContainer.tsx
@@ -0,0 +1,29 @@
+import ProCard from "@ant-design/pro-card"
+import { PageContainer } from "@ant-design/pro-layout"
+
+type CustomPageContainerProps = {
+    children?: JSX.Element
+    title: string
+}
+
+
+const CustomPageContainer = ({ children, title = "" }: CustomPageContainerProps) => {
+    return (
+        <PageContainer
+            childrenContentStyle={{ paddingInline: 16, }}
+            title={title}
+        >
+            <ProCard
+                style={{
+                    minHeight: '100vh',
+                    borderRadius: "20px",
+                    boxShadow: "0 2px 10px rgba(0,0,0,0.2)"
+                }}
+            >
+                {children}
+            </ProCard>
+        </PageContainer>
+    )
+}
+
+export default CustomPageContainer
---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,51 @@
+import { GithubFilled } from '@ant-design/icons';
+import { DefaultFooter, ProLayout } from '@ant-design/pro-components';
+import { ProConfigProvider } from '@ant-design/pro-provider';
+import { useState } from 'react';
+import layoutProps from './layoutProps';
+import FloatActionButton from '../Float/FloatActionButton';
+import Settings from '../../screen/Settings';
+import Home from '../../screen/Home';
+import Category from '../../screen/Category';
+import Task from '../../screen/Task';
+
+
+export default () => {
+    const [pathname, setPathname] = useState<string>('/home');
+    const [theme, setTheme] = useState(true);
+
+    return (
+        <ProConfigProvider dark={theme}>
+            <ProLayout
+                style={{ width: "100%" }}
+                footerRender={() => <DefaultFooter copyright="2023 by LeleDallas All Rights Reserved" />}
+                {...layoutProps}
+                location={{ pathname }}
+                avatarProps={{
+                    src: 'https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg',
+                    size: 'default',
+                    title: 'UserName',
+                }}
+                actionsRender={() => [
+                    <GithubFilled key="GithubFilled"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            window.location.href = 'https://github.com/LeleDallas/TODO-list';
+                        }} />]
+                }
+                menuItemRender={(item, dom) => (
+                    <a onClick={() => setPathname(item.path || '/home')}>
+                        {dom}
+                    </a>
+                )}
+            >
+                {pathname === "/" && <Home />}
+                {pathname === "/home" && <Home />}
+                {pathname === "/task" && <Task />}
+                {pathname === "/categories" && <Category />}
+                {pathname === "/settings" && <Settings theme={theme} setTheme={setTheme} />}
+                <FloatActionButton />
+            </ProLayout>
+        </ProConfigProvider>
+    );
+};
