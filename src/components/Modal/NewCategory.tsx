bdcdf4dd100df82a6a7ede824a3fe574f7c1c222
Dallas <71103219+LeleDallas@users.noreply.github.com>
2023-11-10 18:09:24 +0100

    refactor: update modals

--- src/components/Modal/NewCategory.tsx
+++ src/components/Modal/NewCategory.tsx
@@ -3,9 +3,13 @@ import ProForm, { ModalForm, ProFormText } from "@ant-design/pro-form"
 import { ColorPicker, message } from "antd";
 import { useState } from "react";
 import { Color } from "antd/es/color-picker";
+import { storeData } from "../../utils";
+import { reloadState, setLoad } from "../../reducers";
+import { useAppDispatch } from "../../hooks";
 
 const NewCategory = ({ visible, setVisible }: ModalType) => {
     const [color, setColor] = useState<Color | string>("#1677FF");
+    const dispatch = useAppDispatch()
     return (
         <ModalForm
             requiredMark
@@ -19,21 +23,24 @@ const NewCategory = ({ visible, setVisible }: ModalType) => {
             submitTimeout={2000}
             onFinish={async (values) => {
                 const title = values.title
-                const categoryList = localStorage.getItem("categories")
-                const existingCategories = categoryList ? JSON.parse(categoryList) : {}
-                existingCategories[title] = {
-                    title,
-                    color,
-                    todo: []
-                }
-                localStorage.setItem("categories", JSON.stringify(existingCategories));
-                message.success('New Category Added');
-                return true;
+                const categoryList = localStorage.getItem("categoryColors")
+                const existingCategories: CategoryColors = categoryList ? new Map(Object.entries(JSON.parse(categoryList))) : new Map() as CategoryColors
+                if (existingCategories.has(title)) return
+                dispatch(setLoad(true))
+                existingCategories.set(title, color.toString())
+                storeData("categoryColors", existingCategories)
+                dispatch(reloadState())
+                setVisible(false)
+                setTimeout(() => {
+                    dispatch(setLoad(false))
+                    message.success('New Category Added');
+                }, 1200);
             }}
             style={{ marginTop: 22 }}
         >
             <ProForm.Group align="center" >
                 <ProFormText
+                    rules={[{ required: true, message: 'Please enter a Category Title!' }]}
                     required
                     width="md"
                     name="title"
